2b462b4f808336db22376e4433afcf545bd9cdd2
Isabelle Isa <isabelle.say.isa@gmail.com>
2024-08-26 15:53:32 -0300

    conexao.php feita

--- WEB_2.5/Model/conexao.php
+++ WEB_2.5/Model/conexao.php
@@ -1,5 +1,18 @@
 <?php
 
+$server = 'localhost';
+$dbname = 'bdtcc';
+$user = 'root';
+$password = '';
 
+try {
+	$conn = new PDO("mysql:host=$server;dbname=$dbname", $user, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
+    echo "Conexão estabelecida com sucesso!";
+
+} catch(PDOException $e) {
+    die("Erro: " . $e->getMessage());
+}
+ 
 ?>
