2c09b20c64ad9da5739c14c98daffdf5271b6fab
victhor coppola <victhorcm@gmail.com>
2024-11-24 21:16:50 -0300

    D:

--- WEB_2.5/View/Mda.php
+++ WEB_2.5/View/Mda.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SinaLibras</title>
-    <link rel="stylesheet" type="text/css" href="CSS/Mda.css">
-    <link rel="stylesheet" type="text/css" href="CSS/Parte2Mda.css">
+    <link rel="stylesheet" type="text/css" href="Mda.css">
+    <link rel="stylesheet" type="text/css" href="Parte2Mda.css">
     <link rel="website icon" type="icon" href="CSS/IMAGEM/Favicon/favicon.ico">
 </head>
 <body>
@@ -55,20 +55,25 @@
 </div>
 
 <div class="manchete">
-    <h2>Módulo do curso “Libras Online: Comunicação Acessível e Prática</h2>
-    <p>Neste módulo, vamos explorar os principais conceitos e práticas de Libras (Língua Brasileira de Sinais), uma língua de importância fundamental para a inclusão social e comunicação entre surdos e ouvintes. A proposta deste curso online é oferecer uma abordagem prática e interativa, permitindo que você se familiarize com os sinais e a cultura surda, além de realizar atividades práticas que estimulam o aprendizado dinâmico. A atividade prática deste módulo envolverá o uso de um scanner para identificar sinais e gerar traduções em tempo real, proporcionando uma experiência mais imersiva e eficaz.
+    <h2>Módulo do curso “Libras Online: Comunicação Acessível e Prática</h2><br>
+    <p>Neste módulo, vamos explorar os principais conceitos e práticas de Libras (Língua Brasileira de Sinais), uma língua de importância fundamental para a inclusão social e comunicação entre surdos e ouvintes. A proposta deste curso online é oferecer uma abordagem prática e interativa, permitindo que você se familiarize com os sinais e a cultura surda, além de realizar atividades práticas que estimulam o aprendizado dinâmico. A atividade prática deste módulo envolverá o uso de um scanner para identificar sinais e gerar traduções em tempo real, proporcionando uma experiência mais imersiva e eficaz.</p><br>
 
-O que é Libras e como ela funciona Libras é a língua de sinais usada pela comunidade surda no Brasil, com uma gramática própria e uma estrutura visual e gestual única. Diferente da Língua Portuguesa, Libras é uma língua visual-espacial, onde a comunicação se dá por sinais com as mãos, expressões faciais e movimentos corporais. Neste módulo, você aprenderá não apenas o vocabulário básico, mas também as estruturas gramaticais de Libras, facilitando a comunicação fluida entre surdos e ouvintes em diferentes contextos.
+    <h2>O que é Libras e como ela funciona</h2><br><br>
+    <p>Libras é a língua de sinais usada pela comunidade surda no Brasil, com uma gramática própria e uma estrutura visual e gestual única. Diferente da Língua Portuguesa, Libras é uma língua visual-espacial, onde a comunicação se dá por sinais com as mãos, expressões faciais e movimentos corporais. Neste módulo, você aprenderá não apenas o vocabulário básico, mas também as estruturas gramaticais de Libras, facilitando a comunicação fluida entre surdos e ouvintes em diferentes contextos.</p><br>
 
-Importância da Libras na comunicação inclusiva Compreender a relevância de Libras vai além de aprender os sinais; trata-se de uma mudança de mentalidade para promover a inclusão social. Neste módulo, discutiremos como a Libras é essencial para quebrar barreiras de comunicação, permitindo que a comunidade surda tenha voz e participação ativa nas interações sociais, educacionais e profissionais. Abordaremos também a importância da acessibilidade e da legislação que assegura os direitos linguísticos da comunidade surda no Brasil.
+    <h2>Importância da Libras na comunicação inclusiva</h2> <br>
+    <P>Compreender a relevância de Libras vai além de aprender os sinais; trata-se de uma mudança de mentalidade para promover a inclusão social. Neste módulo, discutiremos como a Libras é essencial para quebrar barreiras de comunicação, permitindo que a comunidade surda tenha voz e participação ativa nas interações sociais, educacionais e profissionais. Abordaremos também a importância da acessibilidade e da legislação que assegura os direitos linguísticos da comunidade surda no Brasil.</P><br>
 
-A atividade prática: escaneando sinais Uma das atividades principais deste módulo será a utilização de um scanner para identificar sinais em Libras e gerar traduções automáticas em texto. Você será orientado a usar um aplicativo de escaneamento que reconhece imagens e sinais feitos com as mãos. O scanner ajudará a traduzir essas imagens em tempo real para palavras escritas, facilitando a compreensão do que está sendo comunicado. Esta atividade permite uma aprendizagem mais interativa e visual, essencial para o domínio da língua de sinais.
+    <h2>A atividade prática: escaneando sinais</h2><br><br>
+    <p>Uma das atividades principais deste módulo será a utilização de um scanner para identificar sinais em Libras e gerar traduções automáticas em texto. Você será orientado a usar um aplicativo de escaneamento que reconhece imagens e sinais feitos com as mãos. O scanner ajudará a traduzir essas imagens em tempo real para palavras escritas, facilitando a compreensão do que está sendo comunicado. Esta atividade permite uma aprendizagem mais interativa e visual, essencial para o domínio da língua de sinais.</P><br>
 
-Dicas práticas para aprimorar a comunicação em Libras A prática constante é fundamental para a aquisição de Libras. Ao longo deste módulo, você será incentivado a realizar atividades diárias de prática, como treinar os sinais, fazer anotações de novos sinais aprendidos e interagir com outros alunos através de vídeos e atividades práticas. Também será importante se familiarizar com os contextos culturais e sociais em que a Libras é utilizada, como eventos, reuniões e situações cotidianas, para entender o papel da linguagem em diferentes cenários.
+    <h2>Dicas práticas para aprimorar a comunicação em Libras</h2><br>
+    <p>A prática constante é fundamental para a aquisição de Libras. Ao longo deste módulo, você será incentivado a realizar atividades diárias de prática, como treinar os sinais, fazer anotações de novos sinais aprendidos e interagir com outros alunos através de vídeos e atividades práticas. Também será importante se familiarizar com os contextos culturais e sociais em que a Libras é utilizada, como eventos, reuniões e situações cotidianas, para entender o papel da linguagem em diferentes cenários.</P><br>
 
-Libras e as tecnologias assistivas Além do scanner, outras tecnologias assistivas serão apresentadas, como aplicativos de tradução e reconhecimento de voz, que ajudam na comunicação entre pessoas surdas e ouvintes. Este módulo irá explorar como essas ferramentas podem ser aliadas na aprendizagem e no uso cotidiano de Libras, promovendo uma comunicação mais fluida e acessível.
+    <h2>Libras e as tecnologias assistivas</h2> <br>
+    <p>Além do scanner, outras tecnologias assistivas serão apresentadas, como aplicativos de tradução e reconhecimento de voz, que ajudam na comunicação entre pessoas surdas e ouvintes. Este módulo irá explorar como essas ferramentas podem ser aliadas na aprendizagem e no uso cotidiano de Libras, promovendo uma comunicação mais fluida e acessível.</P><br>
 
-Ao final deste módulo, você terá uma compreensão sólida da língua de sinais, aprenderá sinais essenciais para interagir de forma inclusiva e será capaz de utilizar a tecnologia como aliada na sua prática de Libras. Este módulo é essencial para qualquer pessoa interessada em aprender Libras de forma eficaz e inclusiva, aprimorando a comunicação e o entendimento mútuo entre as pessoas surdas e ouvintes.</p>
+    <p>Ao final deste módulo, você terá uma compreensão sólida da língua de sinais, aprenderá sinais essenciais para interagir de forma inclusiva e será capaz de utilizar a tecnologia como aliada na sua prática de Libras. Este módulo é essencial para qualquer pessoa interessada em aprender Libras de forma eficaz e inclusiva, aprimorando a comunicação e o entendimento mútuo entre as pessoas surdas e ouvintes.</p>
 </div>
 
         <script src="JS-View/BarradeMenu.js"></script>
